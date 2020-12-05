97b18f52b520f401da7b1d8a2d969686a8b0d0dc
蔡雨鑫 <1261148380@qq.com>
2020-12-05 10:45:32 +0800

    cyx第一次提交

---
+++ src/main/java/com/entity/User.java
@@ -0,0 +1,18 @@
+package com.entity;
+
+/**
+ * 作者：cyx
+ * 日期: 2020/12/5 10:38
+ * 描述:
+ */
+public class User {
+    private String Username;
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+}
